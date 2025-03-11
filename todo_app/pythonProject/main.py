f08665a93146969d88735b372453960fd7b817bc
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-03-10 20:17:33 -0600

    Add files via upload

--- todo_app/pythonProject/main.py
+++ todo_app/pythonProject/main.py
@@ -1,4 +1,12 @@
 todos = []
-user_text = input('Enter a todo: ')
-todos.append(user_text)
-print(todos)
+while True:
+    user_text = input('Type add or show: ')
+    match user_text:
+        case 'add':
+            todo = input('Enter a todo: ')
+            todos.append(user_text)
+        case 'show':
+            for i in todos:
+                print(i)
+        case 'exit':
+            break

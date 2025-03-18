ce8520be7b08810a812fe9e7e17bd39b22b90caa
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-03-18 16:52:17 -0600

    Add files via upload

--- todo_app/pythonProject/main.py
+++ todo_app/pythonProject/main.py
@@ -1,7 +1,7 @@
 while True:
     user_text = input('Type add, edit, complete or show: ').strip()
 
-    if 'add' in user_text:
+    if user_text.startswith('add'):
         todo = user_text[4:] + '\n'
 
         with open('todos.txt', 'r') as file:
@@ -12,37 +12,45 @@ while True:
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
-    elif 'show' in user_text:
+    elif user_text.startswith('show'):
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
         for i, j in enumerate(todos):
             print(f'{i + 1}. {j.strip('\n')}')
 
-    elif 'edit' in user_text:
-        todo_to_edit = int(user_text[5:]) - 1
+    elif user_text.startswith('edit'):
+        try:
+            todo_to_edit = int(user_text[5:]) - 1
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-        new_todo = input('Enter new todo: ')
-        todos[todo_to_edit] = new_todo + '\n'
+            new_todo = input('Enter new todo: ')
+            todos[todo_to_edit] = new_todo + '\n'
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print('Your command is not valid.')
+            continue
 
-    elif 'complete' in user_text:
-        completed_todo = int(user_text[9:]) - 1
+    elif user_text.startswith('complete'):
+        try:
+            completed_todo = int(user_text[9:]) - 1
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-        todos.pop(completed_todo)
+            todos.pop(completed_todo)
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        except IndexError:
+            print('That item is out of range.')
+            continue
 
-    elif 'exit' in user_text:
+    elif user_text.startswith('exit'):
         break
     else:
         print('Command is not valid.')
